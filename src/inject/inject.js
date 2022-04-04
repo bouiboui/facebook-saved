0e87e75160666d85503cbe5bed21b60cd9422cfa
Julien <dev@cod3.net>
2022-04-04 06:33:24 +0200

    Updated dependencies and added Prettier

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,26 +1,25 @@
 let scrollHandle;
 
 const findLinks = () => {
-  const res = findLinksUniversal(document)
-  chrome.extension.sendMessage({foundLinks: res});
-}
+  const res = findLinksUniversal(document);
+  chrome.extension.sendMessage({ foundLinks: res });
+};
 
-chrome.extension.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    if (request.doIt) {
+chrome.extension.onMessage.addListener((request) => {
+  if (request.doIt) {
+    findLinks();
+  }
+  if (request.autoScroll) {
+    if (request.autoScroll === "start") {
+      clearInterval(scrollHandle);
+      scrollHandle = setInterval(() => {
+        scrollTo(0, document.body.scrollHeight);
+        findLinks();
+      }, 500);
+    }
+    if (request.autoScroll === "stop") {
+      clearInterval(scrollHandle);
       findLinks();
     }
-    if (request.autoScroll) {
-      if (request.autoScroll === "start") {
-        clearInterval(scrollHandle);
-        scrollHandle = setInterval(() => {
-          scrollTo(0, document.body.scrollHeight);
-          findLinks();
-        }, 500);
-      }
-      if (request.autoScroll === "stop") {
-        clearInterval(scrollHandle);
-        findLinks();
-      }
-    }
-  });
+  }
+});
--- src/page_action/page_action.js
+++ src/page_action/page_action.js
@@ -1,109 +1,112 @@
 let linksFound;
 
-const linksFoundText = () => document.querySelector('[name="exportType"]:checked').value === 'urls+titles'
-  ? linksFound.map(([url, title]) => `${url} ${title}`).join('\n')
-  : linksFound.map(([url]) => url).join('\n')
+const linksFoundText = () =>
+  document.querySelector('[name="exportType"]:checked').value === "urls+titles"
+    ? linksFound.map(([url, title]) => `${url} ${title}`).join("\n")
+    : linksFound.map(([url]) => url).join("\n");
 
-const linksFoundCSV = () => document.querySelector('[name="exportType"]:checked').value === 'urls+titles'
-  ? linksFound.map(([url, title]) => `${csvField(url)},${csvField(title)}`).join(CSV_ROW_SEPARATOR)
-  : linksFound.map(([url]) => csvField(url)).join(CSV_ROW_SEPARATOR)
+const linksFoundCSV = () =>
+  document.querySelector('[name="exportType"]:checked').value === "urls+titles"
+    ? linksFound
+        .map(([url, title]) => `${csvField(url)},${csvField(title)}`)
+        .join(CSV_ROW_SEPARATOR)
+    : linksFound.map(([url]) => csvField(url)).join(CSV_ROW_SEPARATOR);
 
-chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
   if (!tabs[0].url.match(/facebook.com\/saved/gis)) {
-    UIkit.modal(document.querySelector('#modal-center')).show();
+    UIkit.modal(document.querySelector("#modal-center")).show();
   } else {
-    UIkit.modal(document.querySelector('#modal-center')).hide();
-    sendMessageToTab({doIt: true});
+    UIkit.modal(document.querySelector("#modal-center")).hide();
+    sendMessageToTab({ doIt: true });
   }
 });
 
 const sendMessageToTab = (message, callback) => {
-  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, message, callback);
   });
 };
 
-const onClick = (selector, callback) => document.querySelector(selector)
-  .addEventListener("click", callback);
+const onClick = (selector, callback) =>
+  document.querySelector(selector).addEventListener("click", callback);
 
-onClick('#doItButton', () => {
-  document.querySelector('#spinner').style.display = "block";
-  sendMessageToTab({doIt: true});
+onClick("#doItButton", () => {
+  document.querySelector("#spinner").style.display = "block";
+  sendMessageToTab({ doIt: true });
 });
 
-onClick('#startAutoScrollButton', () => {
-  document.querySelector('#startAutoScrollButton').style.display = "none";
-  document.querySelector('#stopAutoScrollButton').style.display = "inline";
-  sendMessageToTab({autoScroll: "start"});
+onClick("#startAutoScrollButton", () => {
+  document.querySelector("#startAutoScrollButton").style.display = "none";
+  document.querySelector("#stopAutoScrollButton").style.display = "inline";
+  sendMessageToTab({ autoScroll: "start" });
 });
 
-onClick('#stopAutoScrollButton', () => {
-  document.querySelector('#startAutoScrollButton').style.display = "inline";
-  document.querySelector('#stopAutoScrollButton').style.display = "none";
-  sendMessageToTab({autoScroll: "stop"});
+onClick("#stopAutoScrollButton", () => {
+  document.querySelector("#startAutoScrollButton").style.display = "inline";
+  document.querySelector("#stopAutoScrollButton").style.display = "none";
+  sendMessageToTab({ autoScroll: "stop" });
 });
 
-onClick('#copyTextButton', () => {
-  var dummy = document.createElement("textarea");
+onClick("#copyTextButton", () => {
+  const dummy = document.createElement("textarea");
   document.body.appendChild(dummy);
   dummy.value = linksFoundText();
   dummy.select();
   document.execCommand("copy");
   document.body.removeChild(dummy);
-  UIkit.notification('Copied to clipboard!', {pos: 'top-center'});
+  UIkit.notification("Copied to clipboard!", { pos: "top-center" });
 });
 
-onClick('#copyCSVButton', () => {
-  var dummy = document.createElement("textarea");
+onClick("#copyCSVButton", () => {
+  const dummy = document.createElement("textarea");
   document.body.appendChild(dummy);
   dummy.value = linksFoundCSV();
   dummy.select();
   document.execCommand("copy");
   document.body.removeChild(dummy);
-  UIkit.notification('Copied to clipboard!', {pos: 'top-center'});
+  UIkit.notification("Copied to clipboard!", { pos: "top-center" });
 });
 
-onClick('#saveTextButton', () => {
-  var hiddenElement = document.createElement('a');
-  hiddenElement.href = 'data:attachment/text,' + encodeURI(linksFoundText());
-  hiddenElement.target = '_blank';
-  hiddenElement.download = 'saved-links.txt';
+onClick("#saveTextButton", () => {
+  const hiddenElement = document.createElement("a");
+  hiddenElement.href = "data:attachment/text," + encodeURI(linksFoundText());
+  hiddenElement.target = "_blank";
+  hiddenElement.download = "saved-links.txt";
   hiddenElement.click();
-  UIkit.notification('Saved as text file!', {pos: 'top-center'});
+  UIkit.notification("Saved as text file!", { pos: "top-center" });
 });
 
-onClick('#saveCSVButton', () => {
-  var hiddenElement = document.createElement('a');
-  hiddenElement.href = 'data:attachment/csv,' + encodeURI(linksFoundCSV());
-  hiddenElement.target = '_blank';
-  hiddenElement.download = 'saved-links.csv';
+onClick("#saveCSVButton", () => {
+  const hiddenElement = document.createElement("a");
+  hiddenElement.href = "data:attachment/csv," + encodeURI(linksFoundCSV());
+  hiddenElement.target = "_blank";
+  hiddenElement.download = "saved-links.csv";
   hiddenElement.click();
-  UIkit.notification('Saved as CSV file!', {pos: 'top-center'});
+  UIkit.notification("Saved as CSV file!", { pos: "top-center" });
 });
 
-onClick('#openSavedPage', () => {
-  chrome.tabs.query({url: 'https://*.facebook.com/saved*'}, tabs => {
+onClick("#openSavedPage", () => {
+  chrome.tabs.query({ url: "https://*.facebook.com/saved*" }, (tabs) => {
     if (tabs.length > 0) {
       chrome.tabs.reload(tabs[0].id, () => {
-        chrome.tabs.update(tabs[0].id, {active: true})
-      })
+        chrome.tabs.update(tabs[0].id, { active: true });
+      });
     } else {
-      chrome.tabs.create({url: 'https://www.facebook.com/saved'});
-    }
-  })
-});
-
-const CSV_ROW_SEPARATOR = "\n"
-
-const csvField = str => `"${str.replace(/"/gis, '""')}"`
-
-chrome.extension.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    document.querySelector('#spinner').style.display = "none";
-    document.querySelector('#foundLinksCountSpan').innerHTML = "0";
-    if (request.foundLinks) {
-      const res = request.foundLinks;
-      document.querySelector('#foundLinksCountSpan').innerHTML = res.length;
-      linksFound = res;
+      chrome.tabs.create({ url: "https://www.facebook.com/saved" });
     }
   });
+});
+
+const CSV_ROW_SEPARATOR = "\n";
+
+const csvField = (str) => `"${str.replace(/"/gis, '""')}"`;
+
+chrome.extension.onMessage.addListener((request) => {
+  document.querySelector("#spinner").style.display = "none";
+  document.querySelector("#foundLinksCountSpan").innerHTML = "0";
+  if (request.foundLinks) {
+    const res = request.foundLinks;
+    document.querySelector("#foundLinksCountSpan").innerHTML = res.length;
+    linksFound = res;
+  }
+});
